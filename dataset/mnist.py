cbe52f1f262d27b39e9823e6af999d3410c03135
coderyjc <coderyjc@163.com>
2025-03-09 11:05:06 +0800

    update

--- dataset/mnist.py
+++ dataset/mnist.py
@@ -9,10 +9,10 @@ import pickle
 import os
 import numpy as np
 
+# 定义MNIST数据集的下载地址，使用镜像站点
+url_base = 'https://ossci-datasets.s3.amazonaws.com/mnist/'
 
-#url_base = 'http://yann.lecun.com/exdb/mnist/'
-url_base = 'https://ossci-datasets.s3.amazonaws.com/mnist/'  # mirror site
-
+# 定义MNIST数据集中各个文件的名称
 key_file = {
     'train_img':'train-images-idx3-ubyte.gz',
     'train_label':'train-labels-idx1-ubyte.gz',
@@ -20,21 +20,25 @@ key_file = {
     'test_label':'t10k-labels-idx1-ubyte.gz'
 }
 
+# 获取当前脚本所在的目录，并定义保存MNIST数据集的pickle文件路径
 dataset_dir = os.path.dirname(os.path.abspath(__file__))
 save_file = dataset_dir + "/mnist.pkl"
 
+# 定义训练集和测试集的数量，以及图像的维度和大小
 train_num = 60000
 test_num = 10000
 img_dim = (1, 28, 28)
 img_size = 784
 
-
+# 定义一个函数用于下载指定的文件
 def _download(file_name):
     file_path = dataset_dir + "/" + file_name
 
+    # 如果文件已经存在，则直接返回
     if os.path.exists(file_path):
         return
 
+    # 下载文件并保存到指定路径
     print("Downloading " + file_name + " ... ")
     headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"}
     request = urllib.request.Request(url_base+file_name, headers=headers)
@@ -43,10 +47,12 @@ def _download(file_name):
         f.write(response)
     print("Done")
 
+# 定义一个函数用于下载所有MNIST数据集文件
 def download_mnist():
     for v in key_file.values():
        _download(v)
 
+# 定义一个函数用于加载标签数据并将其转换为NumPy数组
 def _load_label(file_name):
     file_path = dataset_dir + "/" + file_name
 
@@ -57,6 +63,7 @@ def _load_label(file_name):
 
     return labels
 
+# 定义一个函数用于加载图像数据并将其转换为NumPy数组
 def _load_img(file_name):
     file_path = dataset_dir + "/" + file_name
 
@@ -68,6 +75,7 @@ def _load_img(file_name):
 
     return data
 
+# 定义一个函数用于将MNIST数据集转换为NumPy数组并存储在字典中
 def _convert_numpy():
     dataset = {}
     dataset['train_img'] =  _load_img(key_file['train_img'])
@@ -77,6 +85,7 @@ def _convert_numpy():
 
     return dataset
 
+# 定义一个函数用于初始化MNIST数据集，包括下载和转换数据
 def init_mnist():
     download_mnist()
     dataset = _convert_numpy()
@@ -85,6 +94,7 @@ def init_mnist():
         pickle.dump(dataset, f, -1)
     print("Done!")
 
+# 定义一个函数用于将标签数据转换为one-hot编码格式
 def _change_one_hot_label(X):
     T = np.zeros((X.size, 10))
     for idx, row in enumerate(T):
@@ -92,30 +102,36 @@ def _change_one_hot_label(X):
 
     return T
 
-
+# 定义一个函数用于加载MNIST数据集，并可选地进行归一化、扁平化和one-hot编码
 def load_mnist(normalize=True, flatten=True, one_hot_label=False):
     
+    # 如果pickle文件不存在，则初始化MNIST数据集
     if not os.path.exists(save_file):
         init_mnist()
 
+    # 从pickle文件中加载数据集
     with open(save_file, 'rb') as f:
         dataset = pickle.load(f)
 
+    # 如果normalize为True，则将图像数据归一化到0-1之间
     if normalize:
         for key in ('train_img', 'test_img'):
             dataset[key] = dataset[key].astype(np.float32)
             dataset[key] /= 255.0
 
+    # 如果one_hot_label为True，则将标签数据转换为one-hot编码格式
     if one_hot_label:
         dataset['train_label'] = _change_one_hot_label(dataset['train_label'])
         dataset['test_label'] = _change_one_hot_label(dataset['test_label'])
 
+    # 如果flatten为False，则将图像数据重塑为4D张量
     if not flatten:
          for key in ('train_img', 'test_img'):
             dataset[key] = dataset[key].reshape(-1, 1, 28, 28)
 
+    # 返回训练集和测试集
     return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label'])
 
-
+# 如果脚本作为主程序运行，则初始化MNIST数据集
 if __name__ == '__main__':
     init_mnist()
